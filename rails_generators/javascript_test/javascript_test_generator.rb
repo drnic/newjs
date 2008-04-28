e45881a5aff9dcd90c8a3239025d89ca6e19f5ef
Dr Nic Williams <drnicwilliams@gmail.com>
2008-04-28 11:54:17 +1000

    javascript_test: nested folders for tests; e.g. javascript_test models/game -> test/javascript/models/game_test.html

--- rails_generators/javascript_test/javascript_test_generator.rb
+++ rails_generators/javascript_test/javascript_test_generator.rb
@@ -1,15 +1,20 @@
 class JavascriptTestGenerator < Rails::Generator::Base
   DEFAULT_SHEBANG = File.join(Config::CONFIG['bindir'],
                               Config::CONFIG['ruby_install_name'])
-  
+
   default_options :author => nil
-  
-  attr_reader :name, :library_name
-  
+
+  attr_reader :path, :name, :library_name
+  attr_reader :nested_folder, :reverse_nested_folder
+
   def initialize(runtime_args, runtime_options = {})
     super
     usage if args.empty?
-    @name = args.shift
+    @path = args.shift
+    name_parts = path.split('/')
+    @name = name_parts.pop
+    @nested_folder = name_parts.first # could be nil if not nested
+    @reverse_nested_folder = (@nested_folder || "").split('/').map { |folder| "../" }.join
     @library_name = args.shift || name
     extract_options
   end
@@ -25,27 +30,28 @@ class JavascriptTestGenerator < Rails::Generator::Base
       m.directory 'test/javascript/assets'
       m.directory 'vendor/plugins/javascript_unittest/lib'
       m.directory 'vendor/plugins/javascript_unittest/tasks'
+      m.directory "test/javascript/#{nested_folder}" if nested_folder
 
       # Create stubs
       m.file     "assets/jsunittest.js", "test/javascript/assets/jsunittest.js"
       m.file     "assets/unittest.css",  "test/javascript/assets/unittest.css"
-      
+
       m.file     "config/javascript_test_autotest.yml.sample",
                   "config/javascript_test_autotest.yml.sample"
 
       m.file     "plugins/javascript_unittest/lib/jstest.rb",
                   "vendor/plugins/javascript_unittest/lib/jstest.rb"
-      m.file     "plugins/javascript_unittest/tasks/runner.rake", 
+      m.file     "plugins/javascript_unittest/tasks/runner.rake",
                   "vendor/plugins/javascript_unittest/tasks/runner.rake"
-      m.file     "plugins/javascript_unittest/tasks/autotest.rake", 
+      m.file     "plugins/javascript_unittest/tasks/autotest.rake",
                   "vendor/plugins/javascript_unittest/tasks/autotest.rake"
-      m.file     "plugins/javascript_unittest/README", 
+      m.file     "plugins/javascript_unittest/README",
                   "vendor/plugins/javascript_unittest/README"
-      m.template "test.html.erb",  "test/javascript/#{name}_test.html"
+      m.template "test.html.erb",  "test/javascript/#{path}_test.html"
 
       %w[rstakeout js_autotest].each do |file|
         m.template "script/#{file}",        "script/#{file}", script_options
-        m.template "script/win_script.cmd", "script/#{file}.cmd", 
+        m.template "script/win_script.cmd", "script/#{file}.cmd",
           :assigns => { :filename => file } if windows
       end
     end
@@ -74,7 +80,7 @@ EOS
       #         "Default: none") { |options[:author]| }
       # opts.on("-v", "--version", "Show the #{File.basename($0)} version number and quit.")
     end
-    
+
     def extract_options
       # for each option, extract it into a local variable (and create an "attr_reader :author" at the top)
       # Templates can access these value via the attr_reader-generated methods, but not the
